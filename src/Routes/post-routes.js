90adef1c8695af79d1db9208d23b3151ad16f462
Will <shaheenwill@gmail.com>
2021-05-25 20:10:13 -0400

    upd

--- src/Routes/post-routes.js
+++ src/Routes/post-routes.js
@@ -4,13 +4,13 @@ const PostController = require('../Controllers/post-controller');
 // post Route - refers to layouts
 router.route('/').get(PostController.showListPostsPage);
 
-router
-    .route('/:id/update')
-        .get(PostController.showPostUpdatePage);
+// router
+//     .route('/:id/update')
+//         .get(PostController.showPostUpdatePage);
 
-router
-    .route('/:id/delete')
-        .get(PostController.showPostDeletePage);
+// router
+//     .route('/:id/delete')
+//         .get(PostController.showPostDeletePage);
 
 router
     .route('/:id')
